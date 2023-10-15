77d0ca29dc204514df752ec8a6c81b2344d6359b
elyikiam <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2023-10-15 16:56:51 +0900

    express links and static

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('All your base are belong to us.');
+});
+
+app.get('/movies', (req, res) => {
+    res.json(top_ten_movies);
+});
+
+app.use(express.static('public'));
+
+app.listen(8080, () => {
+    console.log('Currently listening on port 8080');
+});
