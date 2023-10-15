40e997857552d7035b1c026186d3657e6986a463
elyikiam <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2023-10-15 17:21:24 +0900

    Fixed link and error processing

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express'),
     morgan = require('morgan'),
     fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    top_ten_movies = require('./top_ten_movies.json');
 
 const app = express();
 
@@ -11,12 +12,17 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {f
 //logger setup
 app.use(morgan('combined', {stream: accessLogStream}));
 
+app.use((err, req, res, next) =>{
+    console.error(err.stack);
+    res.status(500).send('Something went wrong.');
+});
+
 app.get('/', (req, res) => {
     res.send('All your base are belong to us.');
 });
 
 app.get('/movies', (req, res) => {
-    res.json(top_ten_movies);
+    res.json(top_ten_movies);   
 });
 
 app.use(express.static('public'));
